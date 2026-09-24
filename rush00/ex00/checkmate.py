b076baa75a93a5ac813888baef213d86131d7c58
khwan67 <meemy.kkiinnoo.x@gmail.com>
2026-09-24 18:49:26 +0700

    rush00

---
+++ rush00/ex00/checkmate.py
@@ -0,0 +1,64 @@
+def checkmate(board_str):
+    
+    board = board_str.strip().split('\n')
+    Row = len(board)
+
+    for r in range(Row):
+        if len(board[r]) != Row:
+            print("Error")
+            return
+        
+    size = len(board)
+    king = None
+    king_count = 0
+    for r in range(size):
+        for c in range(size):
+            if board[r][c] == 'K':
+                king_count += 1
+                king = (r, c)
+                break
+        
+    if king_count != 1 or king_count == 0:
+        print("Error")
+        return
+
+    kr, kc = king
+    chess = ('K', 'Q', 'R', 'B', 'P')
+
+
+    straight_dirs = [(-1, 0), (1, 0), (0, -1), (0, 1)]
+    diagonal_dirs = [(-1, -1), (-1, 1), (1, -1), (1, 1)]
+   
+    for dr, dc in straight_dirs:
+        r, c = kr + dr, kc + dc
+        while r >= 0 and r < size and c >= 0 and c < size:
+            current = board[r][c]
+            if current in ('R', 'Q'):
+                print("Success")
+                return
+            elif current in chess:
+                break  
+            r += dr
+            c += dc
+
+    for dr, dc in diagonal_dirs:
+        r, c = kr + dr, kc + dc
+        while r >= 0 and r < size and c >= 0 and c < size:
+            current = board[r][c]
+            if current in ('B', 'Q'):
+                print("Success")
+                return
+            elif current in chess:
+                break  
+            r += dr
+            c += dc
+
+    
+    pawn = [(kr + 1, kc - 1), (kr + 1, kc + 1)]
+    for r, c in pawn:
+        if r >= 0 and r < size and c >= 0 and c < size:
+            if board[r][c] == 'P':
+                print("Success")
+                return
+
+    print("Fail")
---
+++ rush00/ex00/main.py
@@ -0,0 +1,68 @@
+from checkmate import checkmate
+
+def main():
+
+# R...
+# .K..
+# ..P.
+# ....
+
+# ..
+# .K
+
+# R...
+# .K.....
+# ..P.
+# ....
+
+# R...
+# .K..
+# ..K.
+# ....
+
+# R...
+# ....
+# ..P.
+# ....
+
+# .N..
+# ....
+# ..K.
+# ....
+
+# nnQnn
+# nnnnn
+# nnnnn
+# nnnnn
+# nnKnn
+
+# yY?g&CD
+# M$CV9jH
+# yul9=Zy
+# RUF47AK
+# 8?OCoVJ
+# 31E7j46
+# a?C=mXc
+
+# B????
+# ?????
+# ?????
+# ?????
+# ????K
+
+# .N..
+# .RP.
+# .PK.
+# ....
+    board = """
+B????
+?????
+?????
+?????
+????K
+
+"""
+    checkmate(board)
+
+if __name__ == "__main__":
+    main()
