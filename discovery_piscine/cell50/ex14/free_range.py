d67cf4506efca402b1cd7abe8ae29601cd3fd5bd
khwan67 <meemy.kkiinnoo.x@gmail.com>
2026-09-25 02:16:13 +0700

    finally

---
+++ discovery_piscine/cell50/ex14/free_range.py
@@ -0,0 +1,11 @@
+import sys
+
+if len(sys.argv) == 3:
+    start = int(sys.argv[1])
+    end = int(sys.argv[2])
+
+    numbers = list(range(start, end + 1))
+
+    print(numbers)
+else:
+    print("none")
